fbf39738917b74a2325c96e3e83d999489e8380b
arian-ahamed-emon <emonoficialchanel@gmail.com>
2024-09-09 14:06:08 +0600

    updated

---
+++ lib/water_tracker.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter/material.dart';
+
+class WaterTracker extends StatefulWidget {
+  const WaterTracker({super.key});
+
+  @override
+  State<WaterTracker> createState() => _WaterTrackerState();
+}
+
+class _WaterTrackerState extends State<WaterTracker> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+
+    );
+  }
+}
